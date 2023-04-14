497022cec820c0c5af573536183b7203d4873549
Thanh Lich <lichk2002@gmail.com>
2023-04-14 08:38:41 +0700

    Create a new product attribute Allow multi-order (yes/no)

---
+++ app/code/Tigren/AdvancedCheckout/Setup/InstallData.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tigren\AdvancedCheckout\Setup;
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Eav\Model\Entity\Attribute\Source\Boolean;
+
+class InstallData implements InstallDataInterface
+{
+    private $eavSetupFactory;
+
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'allow_multi_order',
+            [
+                'type' => 'int',
+                'backend' => '',
+                'frontend' => '',
+                'label' => 'Allow Multi-Order',
+                'input' => 'select',
+                'class' => '',
+                'source' => Boolean::class,
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+                'visible' => true,
+                'required' => false,
+                'option' => [
+                    'values' => [
+                        0 => 'No',
+                        1 => 'Yes',
+                    ],
+                ],
+                'user_defined' => false,
+                'default' => 0,
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => false,
+                'used_in_product_listing' => true,
+                'unique' => false,
+                'apply_to' => ''
+            ]
+        );
+    }
+}
